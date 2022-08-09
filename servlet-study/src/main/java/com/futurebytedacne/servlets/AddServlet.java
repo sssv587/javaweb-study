0e32f6b8e910580ff9b64c8cf3e63268c76fe081
sssv587 <1092663295@qq.com>
2022-08-10 00:19:29 +0800

    servlet-解决乱码问题

--- servlet-study/src/main/java/com/futurebytedacne/servlets/AddServlet.java
+++ servlet-study/src/main/java/com/futurebytedacne/servlets/AddServlet.java
@@ -18,6 +18,18 @@ import java.io.IOException;
 public class AddServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //GET方式目前不需要设置编码(基于Tomcat8)
+        //如果是GET请求发送的中文数据，转码稍微有点麻烦(Tomcat8之前)
+        /*
+        //1.将字符串打散成字节数组
+        byte[] bytes = fname.getBytes("ISO-8859-1");
+        //2.将字节数组按照设定的编码重新组装成字符串
+        fname = new String(bytes, "UTF-8");
+         */
+
+        //POST方式下，设置编码，防止中文乱码
+        //需要注意的事，设置编码这一句话代码必须在所有的获取参数动作之前
+        request.setCharacterEncoding("UTF-8");
         String fname = request.getParameter("fname");
         String priceStr = request.getParameter("price");
         Integer price = Integer.parseInt(priceStr);
